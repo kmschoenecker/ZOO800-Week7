6e3738a164d27bc7c353afecd7d403aa3ac5516e
Elizabeth Braatz <elizabethbraatz42@gmail.com>
2025-10-16 15:01:15 -0500

    Eliz - adding updates from my map

--- Elizabeth_map.R
+++ Elizabeth_map.R
@@ -10,3 +10,9 @@ library(dplyr)
 library(maps)
 
 #Pulling in data 
+
+#Go to https://wiatri.net/inventory/bbb/exploreData/theSearchResult.cfm and download an Excel file, then save it as a CSV 
+#We can't use the URL becuase it doesn't all fit in one webpage
+ 
+d = read.csv("bee_data_braatz.csv")
+
