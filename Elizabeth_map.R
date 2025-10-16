a0de833d150bd28e209a0760c72efeea1eb52f74
Elizabeth Braatz <elizabethbraatz42@gmail.com>
2025-10-16 14:57:14 -0500

    starting toa dd t hings to eliz map

--- Elizabeth_map.R
+++ Elizabeth_map.R
@@ -1,4 +1,12 @@
 #### Homework Week 7 ----
 #Elizabeth and Kristine
 
+#############################################################33
 ##Elizabeth's map for data
+#############################################################
+
+#adding our libraries 
+library(dplyr) 
+library(maps)
+
+#Pulling in data 
