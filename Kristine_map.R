9c4966c5c8db9a51f654c90475fe93c4fac9d069
kmschoenecker <kschoenecker@wisc.edu>
2025-10-16 14:59:56 -0500

    field site data for Kristine

--- Kristine_map.R
+++ Kristine_map.R
@@ -2,3 +2,20 @@
 #Elizabeth and Kristine
 
 #Kristine's map
+
+#I had previously worked with a lab member to make some basic site location maps for my data.
+#This code modifies that original output
+
+#Final result: Map of my Wisconsin Field Sites
+
+#Load packages
+library(ggplot2)
+library(ggmap)
+library(maps)
+library(mapdata)
+library(tidyverse)
+library(grid)
+library(ggsn)
+library(ggspatial)
+
+#
